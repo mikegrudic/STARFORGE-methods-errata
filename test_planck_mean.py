df77ed763a81437c7a6bb2d4a53466ffef66d2a3
Mike Grudic <mike.grudic@gmail.com>
2025-11-12 13:40:43 -0500

    code+bib

--- test_planck_mean.py
+++ test_planck_mean.py
@@ -9,7 +9,7 @@ colors = plt.get_cmap("inferno")(np.linspace(0.2, 0.8, len(Tdust_vals)))
 
 for i, (Td1, Td2) in enumerate(Tdust_intervals):
     kappa = dust_mean_opacity(Trad, Td1)
-    fit = np.polyfit(np.log10(Trad), np.log10(kappa), 6)
+    fit = np.polyfit(np.log10(Trad), np.log10(kappa), 9)
     plt.loglog(Trad, kappa, label=r"$T_{\rm d }\in (%g,%g) \rm K$" % (Td1, Td2), color=colors[i])
 
     plt.loglog(Trad, 10.0 ** np.polyval(fit, np.log10(Trad)), ls="dotted", color=colors[i])
@@ -18,7 +18,7 @@ kappa_LTE = dust_mean_opacity(Trad, Trad)
 # kappa_LTE[kappa_LTE == 0] = np.nan
 plt.xlim(2.73, 1e4)
 # plt.ylim(1e-2, 100)
-plt.loglog(Trad, kappa_LTE, label=r"$T_{\rm rad}=T_{\rm dust}$", color="black", ls="dashed")
+plt.loglog(Trad, kappa_LTE, label=r"$T_{\rm rad}=T_{\rm d}$", color="black", ls="dashed")
 plt.ylabel(r"$\kappa_{\rm P} \,\left(\rm cm^{2}\,g^{-1}\right)$")
 plt.xlabel(r"$T_{\rm rad} \,\left(\rm K\right)$")
 legend_args = {"frameon": True, "labelspacing": 0}
@@ -35,7 +35,7 @@ kappa_LTE = dust_mean_opacity(Trad, Trad, which="rosseland")
 # kappa_LTE[kappa_LTE == 0] = np.nan
 plt.xlim(10, 1e4)
 plt.ylim(1e-3, 100)
-plt.loglog(Trad, kappa_LTE, label=r"$T_{\rm rad}=T_{\rm dust}$", color="black", ls="dashed")
+plt.loglog(Trad, kappa_LTE, label=r"$T_{\rm rad}=T_{\rm d}$", color="black", ls="dashed")
 plt.ylabel(r"$\kappa_{\rm R} \,\left(\rm cm^{2}\,g^{-1}\right)$")
 plt.xlabel(r"$T_{\rm rad} \,\left(\rm K\right)$")
 plt.legend(**legend_args)
